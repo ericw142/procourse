5988db7954af102a51e34e788e1b411218e73d06
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-11-23 20:00:54 -0500

    initial layout

--- public/js/members.js
+++ public/js/members.js
@@ -3,6 +3,6 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     console.log(data);
-    $(".member-name").text(`welcome ${data.firstname}  ${data.lastname}`);
+    $(".member-name").text(`${data.firstname}  ${data.lastname}`);
   });
 });
